45dca4c367209cc7c6c8ed80d631dad3825705be
Robert <comps@diprobe.com>
2022-01-03 14:48:23 +0100

    autoISF 2.2
    based on gazelles oref adaption for AAPS

--- FreeAPS/Sources/Models/Preferences.swift
+++ FreeAPS/Sources/Models/Preferences.swift
@@ -9,6 +9,7 @@ struct Preferences: JSON {
     var autoisf: Bool = false
     var autoISFhourlyChange: Decimal = 0.25
     var autoISFmax: Decimal = 1.3
+    var autoISFmin: Decimal = 1
     var smbDeliveryRatio: Decimal = 0.5
     var smbMaxRangeExtension: Decimal = 1
     var rewindResetsAutosens: Bool = true
@@ -57,6 +58,8 @@ struct Preferences: JSON {
     var postMealISFweight: Decimal = 0
     var postMealISFduration: Decimal = 3
     var postMealISFalways: Bool = false
+    var bgAccelISFweight: Decimal = 0
+    var bgBrakeISFweight: Decimal = 0
 }
 
 extension Preferences {
@@ -69,6 +72,7 @@ extension Preferences {
         case autoisf = "use_autoisf"
         case autoISFhourlyChange = "autoisf_hourlychange"
         case autoISFmax = "autoisf_max"
+        case autoISFmin = "autoisf_min"
         case smbDeliveryRatio = "smb_delivery_ratio"
         case smbMaxRangeExtension = "smb_max_range_extension"
         case rewindResetsAutosens = "rewind_resets_autosens"
@@ -116,6 +120,8 @@ extension Preferences {
         case postMealISFweight = "postmeal_ISF_weight"
         case postMealISFduration = "postmeal_ISF_duration"
         case postMealISFalways = "enableppisf_always"
+        case bgAccelISFweight = "bgAccel_ISF_weight"
+        case bgBrakeISFweight = "bgBrake_ISF_weight"
     }
 }
 
